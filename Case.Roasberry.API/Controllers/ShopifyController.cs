c2e1982bffb337a46995fc03e29082cb7ceb818c
Muhammet Andiç <muhammet.andic@gmail.com>
2023-12-26 10:04:49 +0300

    webhook created

--- Case.Roasberry.API/Controllers/ShopifyController.cs
+++ Case.Roasberry.API/Controllers/ShopifyController.cs
@@ -1,5 +1,9 @@
-﻿using Case.Roasberry.Infrastructure.Shopify;
+﻿using Case.Roasberry.Application.Features.Products.Commands.CreateProduct;
+using Case.Roasberry.Infrastructure.Shopify;
+using Case.Roasberry.Infrastructure.Shopify.Models.ApiResponses;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Case.Roasberry.API.Controllers;
 [Route("api/[controller]")]
@@ -7,10 +11,12 @@ namespace Case.Roasberry.API.Controllers;
 public class ShopifyController : ControllerBase
 {
     private readonly IShopifyClient _client;
+    private IMediator _mediator;
 
-    public ShopifyController(IShopifyClient client)
+    public ShopifyController(IShopifyClient client, IMediator mediator)
     {
         _client = client;
+        _mediator = mediator;
     }
 
     [HttpGet("getProducts")]
@@ -23,5 +29,19 @@ public class ShopifyController : ControllerBase
     public async Task GetOrders()
     {
         await _client.GetOrdersAsync();
+    }
+
+    [HttpPost("order-webhook")]
+    public async Task SaveOrders([FromBody] object ordersData)
+    {
+        var product = new CreateProductCommand()
+        {
+            Name = "Deneme",
+            Discount = 0,
+            UnitPrice = 10,
+            Category = "Test",
+            Properties = JsonConvert.SerializeObject(ordersData)
+        };
+        await _mediator.Send(product);
     }
 }
